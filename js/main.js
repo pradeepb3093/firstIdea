fcb292bb841c5761749f4fb8957588e26ecf59bd
pradeep <pbpradeeptechno@gmail.com>
2023-04-13 01:17:07 +0100

    updated website

--- js/main.js
+++ js/main.js
@@ -356,11 +356,13 @@ mailChimp();
 
 
       var cursor = document.getElementById('cursor');
-      document.addEventListener('mousemove', function(e){
-      var x = e.clientX;
-      var y = e.clientY;
-      cursor.style.left = x + 'px';
-      cursor.style.top = y + 'px';
+      if(cursor) {
+        document.addEventListener('mousemove', function(e){
+        var x = e.clientX;
+        var y = e.clientY;
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
       })
+      }
 
 })(jQuery);	
